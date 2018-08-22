bbe9d3b80e9a177d53a0c9c7f27c22b6b9dfe3d8
nimdanor <nimdanor@github.com>
2018-08-22 08:38:52 +0200

    initiale revision

---
+++ concept/integer/constantesentiere.pl
@@ -0,0 +1,58 @@
+
+
+# TODO accents
+
+# oneshot=True
+
+title= Constantes entiere
+
+before=@ intvalues.py
+evaluator=@ intvalues.py
+
+evaluator+=
+b,t = check(response['answer'], V,B)
+t= t+" "+ str(V)+"  "+str(B)
+grade= b,t
+
+==
+
+
+text==
+
+Python permet d'ecrire des constantes entieres dans quatres bases differentes 2,8,10,16.
+
+Soit les bases: Binaire, Octale, Decimale (usuelle), et Hexadecimale.
+
+Pour preciser la base dans laquelle est ecrit la constante une indication doit ^etre donnee :
+
+Les constantes decimales ne commencent pas par Zero.
+
+Les autres commencent par:
+
+1. 0b ou 0B pour les constantes Binaires.
+2. 0o ou 0O pour les constantes Octales.
+3. 0x ou 0X pour les constantes heXadecimales.
+
+==
+
+
+before+=
+import random
+import time
+random.seed(seed)
+
+B=random.choice([2,8,16])
+V=random.randint(2,17)
+text += "Ecrivez la valeur {} en base {} ({})".format(V,B,basename(B))
+
+==
+
+
+
+form==
+<div class="input-group">
+    <input id="form_txt_answer"  class="form-control" placeholder="" style="weigth:150" required>
+</div>
+==
+
+
