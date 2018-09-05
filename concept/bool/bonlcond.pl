a6774fa98d97b51b6765142fbfeb9267cc64559a
nimdanor <nimdanor@github.com>
2018-09-05 15:44:41 +0200

    rendu aleatoire

--- concept/bool/bonlcond.pl
+++ concept/bool/bonlcond.pl
@@ -3,26 +3,56 @@ author=Dominique Revuz
 name=condbool.pl
 title= Condition booléenne  # N'oubliez pas de remplir ce champs svp
 tag= input|boolean|if # N'oubliez pas de remplir ce champs svp
-extends=/template/coding.pl
+template=/template/coding.pl
 text==
-# Easy Jet 
-Ecrire un programme qui demande à l’utilisateur le poids de son bagage en kilos.
-Si son bagage pèse plus de 20 kilos, le programme affichera le message :
+# Super Fly 
+Super Fly est une compagnie aerienne a bas cout qui fait payer un supplement
+en cas de bagages trops lourds.
 
-	Il y a un supplément de 30 euros pour un bagage de plus de 20 kilos.
+Ecrire un programme qui demande à l’utilisateur le poids de son bagage en kilos.  
+```
+{{prompt}}
+```
 
+Si son bagage pèse plus de {{poid}} kilos, le programme affichera le message :  
+```
+Il y a un supplément de {{supplement}} euros pour un bagage de plus de {{poid}} kilos.
+```
+
+Tests: {{input0}},{{input1}},{{input2}}
 ==
 
+before==
+import random 
+
+prompt="Entrez le poid entier du bagage :"
+poid=random.choice([10,20,30,40])
+supplement=random.choice([10,20,30,40])
+code = '''entree= int(input(\"{}\"))'''.format(prompt)
+affiche="Il y a un supplément de {} euros pour un bagage de plus de {} kilos.".format(supplement,poid)
+
+
+
+soluce='''
+{}
+if entree> {}:
+    print("{}")
+
+'''.format(code, poid, affiche)
+
+
+input0=poid+5
+input1=poid-5
+input2=poid*poid
+
+==
 # Choisir pltest ou soluce ou expectedoutput
 
 expectedoutput=Il y a un supplément de 30 euros pour un bagage de plus de 20 kilos.
 
 
-input0=25
-input1=12
 
-@ boolcondsoluce.py [soluce.py]
 
-soluce==
-bob
-==
+
+
+
