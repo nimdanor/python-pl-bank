eb0cedf637aea7abfd4fd88c425ef4175e530fce
dr <dr@pl-test.u-pem.fr>
2018-09-20 17:26:27 +0200

    version 0

---
+++ concept/bool/pltest.pl
@@ -0,0 +1,17 @@
+
+
+
+
+@ pltest_doc.py
+#@ /pysrc/src/plutils.py
+@ evaluator.py [grader.py]
+@ feedback2.py
+@ template.md
+@ /utils/sandboxio.py
+
+builder=@ /template/donobuild.py
+
+editor.fontsize=14px
+# une interface standard d'exercice avec un editeur pour la réponse
+form=@ /python/form/editorform.html
+
