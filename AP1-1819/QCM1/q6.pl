4c4d3c0cd8500cabbc3b224fbda82746ae14a8f2
dr <dr@pl-test.u-pem.fr>
2018-09-30 15:22:59 +0200

    qcm

---
+++ AP1-1819/QCM1/q6.pl
@@ -0,0 +1,34 @@
+
+template=/template/qcm_template.pl
+
+title=Question 5
+
+
+text==
+Qu'affiche le code suivant :
+
+        str_1 = "Bonjour"
+        str_2 = ’ici’
+        bob = str_1 + str_2
+        print(bob)
+
+
+==
+
+nb=4
+nbtrues=1
+uncrosedfalse=
+good==
+Bonjourici
+==
+
+bad==
+0
+Rien
+Bonjour
+ici
+Bonjour ici
+==
+
+feedback=show
+
